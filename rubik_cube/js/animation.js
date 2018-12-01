34351aaaf9bfb9b377d1bac0911afc555d6977c6
achanlin <achanlin@users.noreply.github.com>
2018-11-30 19:22:34 -0500

    Add files via upload

--- rubik_cube/js/animation.js
+++ rubik_cube/js/animation.js
@@ -20,15 +20,34 @@
 		options.animation.delay = 500;
        	var move = e.target.value;
 		cube.execute(move);}
-	
+		
+		
 	function runContinuously(){
 		options.animation.delay = 1000;
 		var move= document.getElementById("run").value;
 		cube.execute(move);}
+		
 	function getInitialStatus(){
 		options.animation.delay = 100;
 		var iniStatus = document.getElementById("ini").name;
-		cube.execute(iniStatus);}	
+		cube.execute(iniStatus);}
+		
+	function getInitialStatus2(){
+		options.animation.delay = 100;
+		var iniStatus = document.getElementById("ini2").name;
+		cube.execute(iniStatus);}
+
+		
 	function refresh_the_page() {
-    location.reload();
-	}
+    location.reload();}
+		
+	function messItUpString(){
+		var possible = ["U","u","R","r","D","d","L","l","F","f","B","b","M","E","S","X","Y","Z", "U'","u'","R'","r'","D'","d'","L'","l'","F'","f'","B'","b'","M'","E'","S'","X'","Y'","Z'","U2","u2","R2","r2","D2","d2","L2","l2","F2","f2","B2","b2","M2","E2","S2","X2","Y2","Z2"];
+		var text="";
+		for (var i = 0; i < 10; i++) {
+			var randomInt = Math.floor(Math.random() * 45);
+			text +=possible[randomInt];}
+		return text;}
+	
+	
+	
